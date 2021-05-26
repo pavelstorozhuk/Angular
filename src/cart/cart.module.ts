ab94a4018fb63096e6e775f1e3b0ecccc1a5cb23
upskill js.basics <upskill.js.basics@gmail.com>
2021-05-26 14:55:57 +0300

    task2-3-comments

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -12,7 +12,7 @@ import {SharedModule} from '../shared/shared.module';
     CommonModule,
     SharedModule
   ],
-  providers: [CartService],
+  // providers: [CartService], // уже зарегистрирован
   exports: [CartListComponent]
 })
 export class CartModule { }
